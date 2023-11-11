713c20e8ce0349d9f868e4f6f496691a87781e8a
AshrafSharafCS <ashrafsharafcs@gmail.com>
2023-11-11 13:36:03 +0000

    handling user error in opentab

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import validators
 #Greeting the user
 print("Welcome to the advanced browser tabs simulation")
 
@@ -7,7 +8,12 @@ tabs=[]
 def MainMenu():
   print("\n1. Open Tab \n2. Close Tab \n3. Switch Tab\n4. Display All Tabs \n5. Open Nested Tab \n6. Clear All Tabs \n7. Save Tabs \n8. Import Tabs \n9. Exit \n- - - - - - - - - - - - - - -")
 
+#OpenTab function takes title and URL as parameters to add the tab to the tabs list and checks if any input is invalid to return invalid otherwise
 def OpenTab(title,URL):
+  validate=validators.url(URL)
+  if validate is True :
     dic={"Title":title , "URL":URL, "NestedTabs":[]}
     tabs.append(dic)
     return "Tab Added Successfully"
+  else:
+    return "Invalid URL"
