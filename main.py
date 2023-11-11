c0504be207e2e8e85df35af19f533c562bc7e6d9
AshrafSharafCS <ashrafsharafcs@gmail.com>
2023-11-11 13:31:08 +0000

    greeting the user and implementing main menu function

---
+++ main.py
@@ -0,0 +1,6 @@
+#Greeting the user
+print("Welcome to the advanced browser tabs simulation")
+
+#Function handles the main menu options
+def MainMenu():
+  print("\n1. Open Tab \n2. Close Tab \n3. Switch Tab\n4. Display All Tabs \n5. Open Nested Tab \n6. Clear All Tabs \n7. Save Tabs \n8. Import Tabs \n9. Exit \n- - - - - - - - - - - - - - -")
