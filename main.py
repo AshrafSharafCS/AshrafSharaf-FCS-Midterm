24a8f6d88e42ba21f26e687251041fe5aa6098e7
AshrafSharafCS <ashrafsharafcs@gmail.com>
2023-11-11 13:34:32 +0000

    adding opentab function

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 #Greeting the user
 print("Welcome to the advanced browser tabs simulation")
 
+#initialize the list tabs
+tabs=[]
 #Function handles the main menu options
 def MainMenu():
   print("\n1. Open Tab \n2. Close Tab \n3. Switch Tab\n4. Display All Tabs \n5. Open Nested Tab \n6. Clear All Tabs \n7. Save Tabs \n8. Import Tabs \n9. Exit \n- - - - - - - - - - - - - - -")
+
+def OpenTab(title,URL):
+    dic={"Title":title , "URL":URL, "NestedTabs":[]}
+    tabs.append(dic)
+    return "Tab Added Successfully"
